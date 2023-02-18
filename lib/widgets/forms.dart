7007d1f5aea1856a71f1c9a2ee6e5b4b9f1c975b
alvitoaditya19 <itoa47@gmail.com>
2023-02-18 20:05:19 +0700

    revision on sign in page

---
+++ lib/widgets/forms.dart
@@ -0,0 +1,53 @@
+
+import 'package:flutter/material.dart';
+import 'package:tflite_image_classification/shared/theme.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+
+    required this.label,
+    this.isShowTitle = true,
+    this.keyboardType = TextInputType.text,
+    this.isObscure = false,
+    this.isHaveColor = false,
+    this.backgroundColor = Colors.transparent,
+    this.isHaveBorderSide = true,
+    this.controller,
+  });
+
+  final String label;
+  final TextInputType keyboardType;
+  final bool isObscure;
+  final bool isShowTitle;
+  final bool isHaveColor;
+  final Color backgroundColor;
+  final bool isHaveBorderSide;
+  final TextEditingController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (isShowTitle)
+          Text(
+            label,
+            style: blackTextStyle.copyWith(
+              fontWeight: medium,
+              fontSize: 14,
+            ),
+          ),
+        if (isShowTitle)
+          const SizedBox(
+            height: 8,
+          ),
+        TextFormField(
+          controller: controller,
+          keyboardType: keyboardType,
+          obscureText: isObscure,
+       
+        ),
+      ],
+    );
+  }
+}
