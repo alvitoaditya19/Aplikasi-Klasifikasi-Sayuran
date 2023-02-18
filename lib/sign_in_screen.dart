84287edd1be13b78e27159dff7efe6372a287ee9
alvitoaditya19 <itoa47@gmail.com>
2023-02-18 20:10:42 +0700

    add sign in page

---
+++ lib/sign_in_screen.dart
@@ -0,0 +1,108 @@
+
+import 'package:flutter/material.dart';
+import 'package:tflite_image_classification/shared/theme.dart';
+import 'package:tflite_image_classification/widgets/buttons.dart';
+import 'package:tflite_image_classification/widgets/forms.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        children: [
+          Container(
+            margin: const EdgeInsets.only(
+              top: 100,
+              bottom: 100,
+            ),
+            width: 155,
+            height: 50,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/logo-sha-light.png'),
+              ),
+            ),
+          ),
+          Text(
+            "Sign In &\nGrow Your Finance",
+            style: blackTextStyle.copyWith(
+              fontSize: 20,
+              fontWeight: semiBold,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            padding: const EdgeInsets.all(22),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: kWhiteColor,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CustomTextFormField(
+                  label: "Email Address",
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const CustomTextFormField(
+                  label: "Password",
+                  isObscure: true,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      minimumSize: Size.zero,
+                      padding: EdgeInsets.zero,
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    child: Text(
+                      "Forgot Password",
+                      style: greyTextStyle.copyWith(
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomFilledButton(
+                  title: "Sign In",
+                  onPressed: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                      context,
+                      "/home",
+                      (route) => false,
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          CustomTextButton(
+            title: "Create New Account",
+            onPressed: () {
+              Navigator.pushNamed(context, "/sign-up");
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
